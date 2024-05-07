c96e0b960c3f86739416bcbfcc749c3e724d734d
Aiker Ary Acosta Cantillo <aikerary@gmail.com>
2024-05-06 22:35:45 -0500

    Designing routes for app

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,56 @@
-import {
-    BrowserRouter,
-    Routes,
-    Route,
-    useParams,
-} from "react-router-dom";
-import { Login } from './pages/login/Login'
-import { ShopCar } from './pages/shop-car/ShopCar'
-import { NoFound } from "./pages/no-found/NoFound";
-import { useEffect, useState } from "react";
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import Login from './pages/login/Login';
+import ShopCar from './pages/shop-car/ShopCar';
+import NoFound from './pages/no-found/NotFound';
+import axios from 'axios';
 
-export const App = () => {
+const App = () => {
+  const [isLogged, setIsLogged] = useState(false);
+  const [isAuthChecked, setIsAuthChecked] = useState(false);
 
-    const [isLogin, setIsLogin] = useState(false)
-    const parametros = useParams()
+  useEffect(() => {
+    const checkAuth = async () => {
+      try {
+        const token = localStorage.getItem('token');
 
-    useEffect(() => {
-        console.log(parametros);
-    }, [])
+        if (token) {
+          const response = await axios.get('http://localhost:3000/ruta-protegida', {
+            headers: {
+              Authorization: `${token}`,
+            },
+          });
 
-    const miFuncion = (params) => {
-        console.log("desde el padre:", params);
-    }   
+          if (response.status === 200) {
+            setIsLogged(true);
+          } else {
+            localStorage.removeItem('token');
+          }
+        }
+        setIsAuthChecked(true);
+      } catch (err) {
+        console.error(err);
+        localStorage.removeItem('token');
+        setIsAuthChecked(true);
+      }
+    };
 
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Login bus={miFuncion} />} />
-                <Route path="/shop" element={<ShopCar />} />
-                <Route path="*" element={<NoFound />} />
-            </Routes>
-        </BrowserRouter>
-    )
-}
+    checkAuth();
+  }, []);
+
+  if (!isAuthChecked) {
+    return null; // Muestra un spinner de carga u otra interfaz de carga mientras se verifica la autenticación.
+  }
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={isLogged ? <Navigate to="/shop" /> : <Login setIsLogged={setIsLogged} />} />
+        <Route path="/shop" element={isLogged ? <ShopCar /> : <Navigate to="/" />} />
+        <Route path="/*" element={isLogged ? <NoFound /> : <Navigate to="/" />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
